dc5b6c50504cccdde6823ec44cdf5b97fdc61de6
Quejicus <jesusmugo91@gmail.com>
2025-04-09 20:45:03 +0200

    Nuevas correcciones client_secrets

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ else:
 df_combined.to_csv(HISTORICO_FILE, index=True, index_label="id")
 
 gauth = GoogleAuth()
-gauth.LoadCredentialsFile("client_secrets.json")
+gauth.LoadCredentialsFile("mycreds.txt")
 
 if gauth.credentials is None:
     gauth.LocalWebserverAuth()
@@ -221,7 +221,7 @@ elif gauth.access_token_expired:
 else:
     gauth.Authorize()
 
-gauth.SaveCredentialsFile("client_secrets.json")
+gauth.SaveCredentialsFile("mycreds.txt")
 drive = GoogleDrive(gauth)
 
 file_drive = drive.CreateFile({"title": "alhambra_sharan_hist.csv"})
